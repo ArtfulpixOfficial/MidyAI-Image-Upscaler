4f49471d043a6c95f29fd81ebeec440fbfddba09
ArtfulpixOfficial <143751560+ArtfulpixOfficial@users.noreply.github.com>
2024-01-25 15:31:08 -0600

    Add files via upload

---
+++ src/Navbar.jsx
@@ -0,0 +1,43 @@
+import { Button } from "./Button";
+
+export function Navbar({ image, newImage, reset }) {
+  const handleDownload = async function () {
+    if (!newImage) return;
+    const imageUrl = `/proxy-image/${newImage.slice(31)}`;
+
+    const response = await fetch(imageUrl);
+    const blob = await response.blob();
+    const link = document.createElement("a");
+    link.href = URL.createObjectURL(blob);
+    link.download = "generated_image.jpg";
+    link.click();
+  };
+  return (
+    <nav className={image ? "searchNav" : "normal"}>
+      {image ? (
+        <>
+          <Button className="btnBack show" onClick={reset}>{`< Back`}</Button>
+          <Button
+            className="btnDownload show"
+            onClick={handleDownload}
+            disabled={newImage ? false : true}
+          >
+            <svg
+              width="20"
+              height="20"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M3 19L21 19V21L3 21V19ZM13 9L20 9L12 17L4 9L11 9V1L13 1L13 9Z"
+                fill="currentColor"
+              ></path>
+            </svg>
+            Download
+          </Button>
+        </>
+      ) : null}
+    </nav>
+  );
+}
